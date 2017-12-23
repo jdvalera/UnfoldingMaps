0db916c9bb8e2e4a848de579086a50f2c3c10ce7
jdvalera <johnlemuelvalera@yahoo.com>
2017-12-23 15:36:30 -0800

    Completed isLand method

--- src/module4/EarthquakeCityMap.java
+++ src/module4/EarthquakeCityMap.java
@@ -35,7 +35,7 @@ public class EarthquakeCityMap extends PApplet {
 	private static final long serialVersionUID = 1L;
 
 	// IF YOU ARE WORKING OFFILINE, change the value of this variable to true
-	private static final boolean offline = false;
+	private static final boolean offline = true;
 	
 	/** This is where to find the local tiles, for working without an Internet connection */
 	public static String mbTilesString = "blankLight-1-3.mbtiles";
@@ -170,6 +170,9 @@ public class EarthquakeCityMap extends PApplet {
 		// If isInCountry ever returns true, isLand should return true.
 		for (Marker m : countryMarkers) {
 			// TODO: Finish this method using the helper method isInCountry
+			if(this.isInCountry(earthquake, m)) {
+				return true;
+			}
 			
 		}
 		
